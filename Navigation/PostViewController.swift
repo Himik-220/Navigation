f5d468468dcba83339f9047c0c965a8b3501b911
Himik220 <1703_ptz@mail.ru>
2022-11-28 23:21:55 +0300

    create TabBarController, Navigation Feed and Profile controller, Feed and Profile and Post controller

---
+++ Navigation/PostViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PostViewController.swift
+//  Navigation
+//
+//  Created by Leonid Shchipkov on 28.11.2022.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Leonid Shchipkov on 28.11.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+  override func viewDidLoad() {
+      super.viewDidLoad()
+    view.backgroundColor = .yellow
+  }
+}
