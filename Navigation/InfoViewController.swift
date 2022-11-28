2a49f09f300e03506dcd51c6887023ce8a06abb9
Himik220 <1703_ptz@mail.ru>
2022-11-29 00:31:22 +0300

    create info and post and alert controller,

---
+++ Navigation/InfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InfoViewController.swift
+//  Navigation
+//
+//  Created by Leonid Shchipkov on 28.11.2022.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+  
+  let myBytton = UIButton(frame: CGRect(x: 100, y: 200, width: 100, height: 50))
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .gray
+    myBytton.setTitle("alert", for: .normal)
+    myBytton.setTitleColor(.black, for: .normal)
+    myBytton.backgroundColor = .red
+    view.addSubview(myBytton)
+    myBytton.addTarget(self, action: #selector(goAlert), for: .touchUpInside)
+  }
+  
+  @objc func goAlert() {
+    let alert = UIAlertController(title: "", message: navigationController?.viewControllers[1].title, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "Да", style: .default, handler: { _ in
+      print("Press Да")
+    }))
+    alert.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler: {_ in
+      print("Press Отмена")
+    }))
+    self.present(alert, animated: true)
+  }
+}
--- Navigation/PostViewController.swift
+++ Navigation/PostViewController.swift
@@ -8,8 +8,19 @@
 import UIKit
 
 class PostViewController: UIViewController {
-
+  
+  let post = Post(title: "myPost")
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.title = post.title
+    view.backgroundColor = .white
+    let barItem = UIBarButtonItem(title: nil, image: UIImage(systemName: "plus"), target: self, action: #selector(creatInfo))
+    navigationItem.rightBarButtonItem = barItem
+  }
+  
+  @objc func creatInfo() {
+    navigationController?.show(InfoViewController(), sender: nil)
   }
 }
+
