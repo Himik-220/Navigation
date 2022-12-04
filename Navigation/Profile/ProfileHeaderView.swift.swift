5b9754c00c53cb997ac541ccfea14217cb604eee
Himik220 <1703_ptz@mail.ru>
2022-12-04 21:38:00 +0300

    Create HeaderView, change backgroundColor FeedViewController, add header on profileViewController

---
+++ Navigation/Profile/ProfileHeaderView.swift.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Leonid Shchipkov on 04.12.2022.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+  
+  
+}
--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+  
+  let header = ProfileHeaderView()
+  
   override func viewDidLoad() {
       super.viewDidLoad()
-    view.backgroundColor = .yellow
+    view.backgroundColor = .lightGray
+    view.addSubview(header)
+  }
+  
+  override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+    header.frame = view.frame
   }
 }
